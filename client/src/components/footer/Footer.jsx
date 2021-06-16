88bcd72a06f5bcfbef23aaafae77cd50001202b1
spara <sophia.parafina@gmail.com>
2021-06-15 20:34:27 -0500

    initial

---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <em>Nikers</em>
+      &nbsp;&copy;&nbsp;
+      {new Date().getFullYear()}.
+    </footer>
+  );
+};
+
+export default Footer;
